6a87bb87172a37d36d30448eaace2d41000cd858
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-07-31 19:19:24 +0200

    Creata la funzione per aggiungere la classe box-opened all'elemento cliccato;

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,15 +1,18 @@
-// IMPORT
-
-
 // COMPONENT
 export default function Calendar({ source }) {
+
     const iconUrl = "./src/assets/img/icons/";
+
+    // FUNZIONI
+    const boxOpened = event => {
+        event.currentTarget.classList.add('box-opened');
+    };
 
     return (
         <div className="calendar">
             {source.map((t) => {
                 return (
-                    <div key={t.id} className="box">
+                    <div key={t.id} className="box" onClick={boxOpened}>
                         <img src={iconUrl + t.icon + ".png"} alt={t.icon} className="box-icon" />
                         <div className="box-date">{t.id + 1}</div>
                     </div>
