853225c6d4b80af3ec2b8c0ba5e227616a2e8d2a
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-07-31 17:04:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// IMPORT
+import { useState } from 'react'
+import './App.css'
+import Container from './components/Container';
+// _____________________________________
+
+
+function App() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default App
