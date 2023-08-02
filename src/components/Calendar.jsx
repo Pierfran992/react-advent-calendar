0a1227b039e0cbfca73547f3f87abb0d4988f3ee
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-08-02 15:38:00 +0200

    Creata la funzione che assegna al box una classe per identificarlo aperto e invia la sua key nel local storage per farlo rimanere aperto anche dopo il reload della pagina;

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,18 +1,31 @@
 // COMPONENT
 export default function Calendar({ source }) {
-
     const iconUrl = "./src/assets/img/icons/";
+    // array che prende i valori del local storage se ce ne sono presenti o restituisce un array vuoto se non ne ha
+    const storedDay = !!localStorage.getItem('showDays') ? JSON.parse(localStorage.getItem('showDays')) : [];
+
 
     // FUNZIONI
-    const boxOpened = event => {
-        event.currentTarget.classList.add('box-opened');
+    // funzione che assegna al box la classe per identificarlo come aperto e l'ho invia al localstorage controllando se è già presente o no
+    const boxOpened = (e, t) => {
+        const isStoredDay = storedDay.find(elem => elem === t.id + 1);
+        if (!isStoredDay) {
+            e.currentTarget.classList.add('box-opened');
+            storedDay.push(t.id + 1);
+            localStorage.setItem('showDays', JSON.stringify(storedDay))
+            console.log(e);
+            console.log(t);
+        }
     };
+
 
     return (
         <div className="calendar">
             {source.map((t) => {
+                const isStoredDay = storedDay.find(elem => elem === t.id + 1);
                 return (
-                    <div key={t.id} className="box" onClick={boxOpened}>
+                    //{/* !!isStoredDay && isStoredDay.lenght > 0 ? "box box-opened" : "box" */}
+                    <div key={t.id} className={!!isStoredDay ? "box box-opened" : "box"} onClick={(e) => boxOpened(e, t)} >
                         <img src={iconUrl + t.icon + ".png"} alt={t.icon} className="box-icon" />
                         <div className="box-date">{t.id + 1}</div>
                     </div>
